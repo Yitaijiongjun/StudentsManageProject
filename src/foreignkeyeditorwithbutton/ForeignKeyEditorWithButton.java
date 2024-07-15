050934ec4a6e48a8dc3926f1584c953624411bfc
以太窘君 <2105691269@qq.com>
2024-07-16 07:46:08 +0800

    修复了自定义渲染器的选中外观和编辑问题

--- src/foreignkeyeditorwithbutton/ForeignKeyEditorWithButton.java
+++ src/foreignkeyeditorwithbutton/ForeignKeyEditorWithButton.java
@@ -28,7 +28,7 @@ public class ForeignKeyEditorWithButton extends JFrame {
         TableColumn column = table.getColumnModel().getColumn(2);
         // 设置自定义表格单元格呈现器和编辑器
         column.setCellRenderer(new ButtonRenderer());
-        column.setCellEditor(new ButtonEditor(new JCheckBox()));
+        column.setCellEditor(new ButtonEditor(new JTextField()));
 
         JScrollPane scrollPane = new JScrollPane(table);
         add(scrollPane, BorderLayout.CENTER);
@@ -55,62 +55,45 @@ public class ForeignKeyEditorWithButton extends JFrame {
             add(label, BorderLayout.CENTER);
         }
 
-        public Component getTableCellRendererComponent(
-                JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        public Component getTableCellRendererComponent(JTable table, Object value,
+                                                       boolean isSelected, boolean hasFocus,
+                                                       int row, int column) {
             label.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
             label.setText(value == null ? "" : value.toString());
-
+/*
             if (isSelected) {
                 setBackground(table.getSelectionBackground());
             } else {
                 setBackground(table.getBackground());
             }
+
+ */
             return this;
         }
     }
 
     class ButtonEditor extends DefaultCellEditor {
-        private String label;
-        private boolean isPushed;
-        public ButtonEditor(JCheckBox checkBox) {
-            super(checkBox);
+        public ButtonEditor(JTextField textField) {
+            super(textField);
             setClickCountToStart(1);
         }
         public Component getTableCellEditorComponent(JTable table, Object value,
-                                                     boolean isSelected, int row, int column) {
-            label = (value == null) ? "" : value.toString();
+                                                     boolean isSelected,
+                                                     int row, int column) {
+            delegate.setValue(value);
             JPanel panel = new JPanel(new BorderLayout());
-
-            JLabel cellLabel = new DefaultTableCellRenderer();
-            cellLabel.setText(label);
-
             JButton button = new JButton("...");
             button.setMargin(new Insets(0, 0, 0, 0));
+            panel.add(editorComponent, BorderLayout.CENTER);
+            panel.add(button, BorderLayout.EAST);
             button.addActionListener(e -> {
                 fireEditingStopped();
                 showForeignKeyDialog(row, column);
             });
-            panel.add(cellLabel, BorderLayout.CENTER);
-            panel.add(button, BorderLayout.EAST);
-            isPushed = true;
             return panel;
         }
 
-        public Object getCellEditorValue() {
-            isPushed = false;
-            return label;
-        }
-
-        public boolean stopCellEditing() {
-            isPushed = false;
-            return super.stopCellEditing();
-        }
-
-        protected void fireEditingStopped() {
-            super.fireEditingStopped();
-        }
-
-        private void showForeignKeyDialog(int row, int column) {
+        public void showForeignKeyDialog(int row, int column) {
             JDialog dialog = new JDialog(ForeignKeyEditorWithButton.this, "选择院系", true);
             dialog.setSize(400, 300);
             dialog.setLayout(new BorderLayout());
@@ -130,40 +113,40 @@ public class ForeignKeyEditorWithButton extends JFrame {
                     {"LAW", "法学院"}
             };
 
-            DefaultTableModel departmentTableModel = new DefaultTableModel(data, columnNames){
+            DefaultTableModel referenceTableModel = new DefaultTableModel(data, columnNames){
                 @Override
                 public boolean isCellEditable(int rowIndex, int columnIndex) {
                     // 直接返回 false表示所有单元格都不可编辑
                     return false;
                 }
             };
-            JTable departmentTable = new JTable(departmentTableModel);
-            departmentTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-            JScrollPane tableScrollPane = new JScrollPane(departmentTable);
+            JTable referenceTable = new JTable(referenceTableModel);
+            referenceTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            JScrollPane tableScrollPane = new JScrollPane(referenceTable);
 
             JPanel checkBoxPanel = new JPanel(new FlowLayout());
-            JCheckBox showDeptNameCheckBox = new JCheckBox("显示院系全称");
-            showDeptNameCheckBox.setSelected(true);
-            showDeptNameCheckBox.addItemListener(e -> {
-                TableColumnModel columnModel = departmentTable.getColumnModel();
-                TableColumn deptNameColumn = columnModel.getColumn(1);
-                if (showDeptNameCheckBox.isSelected()) {
-                    deptNameColumn.setMinWidth(100);
-                    deptNameColumn.setMaxWidth(200);
-                    deptNameColumn.setPreferredWidth(150);
+            JCheckBox showColumnNameCheckBox = new JCheckBox("显示院系全称");
+            showColumnNameCheckBox.setSelected(true);
+            showColumnNameCheckBox.addItemListener(e -> {
+                TableColumnModel columnModel = referenceTable.getColumnModel();
+                TableColumn selectColumn = columnModel.getColumn(1);
+                if (showColumnNameCheckBox.isSelected()) {
+                    selectColumn.setMinWidth(100);
+                    selectColumn.setMaxWidth(200);
+                    selectColumn.setPreferredWidth(150);
                 } else {
-                    deptNameColumn.setMinWidth(0);
-                    deptNameColumn.setMaxWidth(0);
-                    deptNameColumn.setPreferredWidth(0);
+                    selectColumn.setMinWidth(0);
+                    selectColumn.setMaxWidth(0);
+                    selectColumn.setPreferredWidth(0);
                 }
             });
-            checkBoxPanel.add(showDeptNameCheckBox);
+            checkBoxPanel.add(showColumnNameCheckBox);
 
             JButton selectButton = new JButton("确定");
             selectButton.addActionListener(e -> {
-                int selectedRow = departmentTable.getSelectedRow();
+                int selectedRow = referenceTable.getSelectedRow();
                 if (selectedRow != -1) {
-                    String selectedDepartment = (String) departmentTableModel.getValueAt(selectedRow, 0);
+                    String selectedDepartment = (String) referenceTableModel.getValueAt(selectedRow, 0);
                     tableModel.setValueAt(selectedDepartment, row, column);
                     dialog.dispose();
                 }
